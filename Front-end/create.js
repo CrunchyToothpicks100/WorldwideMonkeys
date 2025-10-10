ac5d1e524f922e64272918b33e2305fe6bd0eb1e
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2025-10-10 10:10:16 -0500

    Moved files to ./Front-End

---
+++ Front-end/create.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("create-form");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault(); // prevent default page reload
+
+    const formData = new FormData(form);
+    const jsonData = Object.fromEntries(formData.entries());
+
+    for (const [key, value] of formData.entries()) {
+        console.log(key, value);
+    }
+
+    // Send JSON to the backend
+    const response = await fetch("https://localhost:5014/api/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonData)
+    });
+
+    const result = await response.json();
+    console.log(result);
+});
