95c7f28e8f616bcc7e099eafbdb51a8becd03b34
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2025-11-25 18:46:05 -0600

    Tweaked menu buttons more

--- Front-end/javascript/user_menu.js
+++ Front-end/javascript/user_menu.js
@@ -14,12 +14,15 @@ function openMenu(evt) {
     const menu = document.createElement('div');
     menu.id = 'user-menu';
     menu.style.position = 'fixed'; // position relative to viewport
+    menu.style.border = '1px solid #d1da49';
     menu.style.top = `${rect.bottom}px`;
-    menu.style.right = '0px';
+    menu.style.right = '40px';
     menu.style.padding = '10px';
-    menu.innerHTML = `<button class="subtle_button" style="color: #d1da49;" onclick="window.location.href='settings.html'">Settings</button><br>
-                      <button class="subtle_button" style="color: #d1da49;" onclick="logout()">Log Out</button>`;
+    menu.innerHTML = `<button class="subtle_button" style="color: #d1da49; padding-inline: 15px;" onclick="window.location.href='settings.html'">Settings</button><br>
+                      <button class="subtle_button" style="color: #d1da49; padding-inline: 15px;" onclick="logout()">Log Out</button>`;
     document.body.appendChild(menu);
+
+    // click away to close menu
     document.addEventListener('click', function handler(event) {
         if (!menu.contains(event.target)) {
             menu.remove();
